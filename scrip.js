90fe5b6412d75b544d6aae48a04d98c9295bcfdc
salome03 <lavallesalome@gmail.com>
2022-10-06 18:41:34 -0300

    Respuesta ejercicio 2

---
+++ scrip.js
@@ -0,0 +1,46 @@
+var nombre= "salome";
+console.log (salome);
+
+let apellido= "Lavalle";
+console.log (apellido);
+
+const curso= "5 1";
+console.log (curso);
+
+function suma (num1, num2){
+    var sumar= num1 + num2;
+    return sumar;
+}
+
+var num1= 2;
+var num2= 4;
+var sumar= suma(num1, num2);
+console.log (suma(num1,num2));
+
+function multiplicacion (num1,num2){
+    let multi= num1*num2;
+ return multi;
+ }
+ var num1= 3;
+ var num2= 6;
+ let multi= multiplicacion (num1,num2);
+ console.log(multiplicacion(num1,num2));
+
+ function resta (num1, num2){
+    let rest = num1-num2;
+    return rest;
+}
+
+var num1= 6;
+var num2= 10;
+let res= resta(num1, num2);
+console.log (resta(num1,num2));
+
+function division (num1, num2){
+    var div= num1/num2;
+    return div;
+}
+var num1= 6;
+var num2= 9;
+let div= division(num1, num2);
+console.log (division(num1,num2));
